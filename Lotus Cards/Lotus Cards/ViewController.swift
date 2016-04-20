4f9232be5908a3f35c8f7e84b86c7d15ec90d3ae
CarpeDiEmma <EyesOnly@client-104-39-84-174.mobility.psu.edu>
2016-04-20 19:34:30 -0400

    Initial Project Commit

---
+++ Lotus Cards/Lotus Cards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lotus Cards
+//
+//  Created by Emma Roudabush on 4/20/16.
+//  Copyright © 2016 Emma Roudabush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
